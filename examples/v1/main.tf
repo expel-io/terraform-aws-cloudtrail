3f38db6834abfab4069cffdd313ef876741cc7ad
Ethan Miller <ethan.miller@expel.io>
2022-02-07 16:35:06 -0500

    format

--- examples/v1/main.tf
+++ examples/v1/main.tf
@@ -3,21 +3,21 @@ provider "aws" {
 }
 
 module "expel_aws_cloudtrail_integration" {
-  source                           = "../../"
+  source = "../../"
 
   expel_aws_account_arn            = "Replace with Expel's AWS account ARN here"
   expel_customer_organization_guid = "Replace with your organization GUID -- (this unique identifier is assigned to you by Expel. You can find it in your browser URL after navigating to Settings > My Organization in Workbench)"
-
-  expel_assume_role_session_name = "ExpelServiceAssumeRoleForCloudTrailAccess"
-  enable_s3_encryption = true
-  enable_sqs_encryption = true
-  queue_message_retention_days = 10
+  expel_assume_role_session_name   = "ExpelServiceAssumeRoleForCloudTrailAccess"
+  enable_s3_encryption             = true
+  enable_sqs_encryption            = true
+  queue_message_retention_days     = 10
 
   prefix = "expel-cloudtrail-integration"
-
-  tags = {}
+  tags = {
+    "is_external" = "true"
+  }
 }
 
-output "integration" {
+output "expel_aws_cloudtrail_integration" {
   value = module.expel_aws_cloudtrail_integration
 }
