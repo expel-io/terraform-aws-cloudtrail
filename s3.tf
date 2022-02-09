fc676b8c34a96243bb12c24c20ab5fc1ba738518
Ethan Miller <ethan.miller@expel.io>
2022-02-09 16:53:08 -0500

    rename files & move s3 to independent file

--- s3.tf
+++ s3.tf
@@ -1,23 +1,3 @@
-# ignoring since cloudtrail is inaccessable beyond integration with S3 which uses encryption by default
-# tfsec:ignore:aws-cloudtrail-enable-at-rest-encryption
-resource "aws_cloudtrail" "cloudtrail" {
-  name                  = "${var.prefix}-cloudtrail"
-  is_multi_region_trail = true
-  is_organization_trail = var.enable_organization_trail
-  s3_bucket_name        = aws_s3_bucket.cloudtrail_bucket.id
-
-  enable_log_file_validation = var.enable_cloudtrail_log_file_validation
-
-  event_selector {
-    include_management_events = true
-    read_write_type           = "All"
-  }
-
-  depends_on = [aws_s3_bucket_policy.cloudtrail_bucket_policy]
-
-  tags = local.tags
-}
-
 resource "aws_s3_bucket" "cloudtrail_bucket" {
   bucket_prefix = var.prefix
   acl           = "private"
