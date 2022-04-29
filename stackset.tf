6ab593a5a1c7febd776d25638789870ab8ded872
Ethan Miller <ethan.miller@expel.io>
2022-04-29 11:35:06 -0400

    Use templatefile func

--- stackset.tf
+++ stackset.tf
@@ -17,26 +17,27 @@ data "aws_iam_policy_document" "AWSCloudFormationStackSetAdministrationRole_assu
 resource "aws_iam_role" "AWSCloudFormationStackSetAdministrationRole" {
   count = var.enable_organization_trail ? 1 : 0
 
-  assume_role_policy = data.aws_iam_policy_document.AWSCloudFormationStackSetAdministrationRole_assume_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.AWSCloudFormationStackSetAdministrationRole_assume_role_policy[0].json
   name               = "AWSCloudFormationStackSetAdministrationRole"
 }
 
 resource "aws_cloudformation_stack_set" "permeate_account_policy" {
   count = var.enable_organization_trail ? 1 : 0
 
-  administration_role_arn = aws_iam_role.AWSCloudFormationStackSetAdministrationRole.arn
-  name                    = "permeate_account_policy"
+  administration_role_arn = aws_iam_role.AWSCloudFormationStackSetAdministrationRole[0].arn
+  name                    = "PermeateAccountPolicy"
 
   auto_deployment {
-	  enabled = true
+    enabled = true
   }
 
-  parameters = {
-    expel_customer_organization_guid = var.expel_customer_organization_guid
-    expel_assume_role_arn = aws_iam_role.expel_assume_role.arn
-  }
-
-  template_body = local.template_body
+  template_body = templatefile(
+    "./stackset.tftpl",
+    {
+      expel_customer_organization_guid = var.expel_customer_organization_guid,
+      expel_assume_role_arn            = aws_iam_role.expel_assume_role.arn
+    }
+  )
 }
 
 data "aws_iam_policy_document" "AWSCloudFormationStackSetAdministrationRole_ExecutionPolicy" {
@@ -45,7 +46,7 @@ data "aws_iam_policy_document" "AWSCloudFormationStackSetAdministrationRole_Exec
   statement {
     actions   = ["sts:AssumeRole"]
     effect    = "Allow"
-    resources = ["arn:aws:iam::*:role/${aws_cloudformation_stack_set.permeate_account_policy.execution_role_name}"]
+    resources = ["arn:aws:iam::*:role/${aws_cloudformation_stack_set.permeate_account_policy[0].execution_role_name}"]
   }
 }
 
@@ -53,6 +54,6 @@ resource "aws_iam_role_policy" "AWSCloudFormationStackSetAdministrationRole_Exec
   count = var.enable_organization_trail ? 1 : 0
 
   name   = "ExecutionPolicy"
-  policy = data.aws_iam_policy_document.AWSCloudFormationStackSetAdministrationRole_ExecutionPolicy.json
-  role   = aws_iam_role.AWSCloudFormationStackSetAdministrationRole.name
+  policy = data.aws_iam_policy_document.AWSCloudFormationStackSetAdministrationRole_ExecutionPolicy[0].json
+  role   = aws_iam_role.AWSCloudFormationStackSetAdministrationRole[0].name
 }
