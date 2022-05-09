ec7825a3a276b1c331ff584300958956cc16a5d9
Ethan Miller <ethan.miller@expel.io>
2022-05-09 13:57:44 -0400

    move aws data vars

--- terraform.tf
+++ terraform.tf
@@ -7,22 +7,3 @@ terraform {
   }
   required_version = ">= 1.1.0"
 }
-
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
-data "aws_organizations_organization" "current" {}
-
-locals {
-  default_tags = {
-    "vendor" = "expel"
-  }
-
-  tags = merge(
-    var.tags,
-    local.default_tags,
-  )
-
-  region                       = data.aws_region.current.name
-  customer_aws_account_id      = data.aws_caller_identity.current.account_id
-  customer_aws_organization_id = try(data.aws_organizations_organization.current.roots[0].id, "")
-}
