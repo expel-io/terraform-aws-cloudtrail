9e1029e1889ce9e6e30cfd7fb6949f7129f4c8ea
Ethan Miller <ethan.miller@expel.io>
2022-05-10 13:44:28 -0400

    Remove unused role & fix typo

--- stackset.tf
+++ stackset.tf
@@ -1,31 +1,8 @@
-data "aws_iam_policy_document" "AWSCloudFormationStackSetAdministrationRole_assume_role_policy" {
-  count = var.enable_organization_trail ? 1 : 0
-
-  statement {
-    actions = ["sts:AssumeRole"]
-    effect  = "Allow"
-
-    principals {
-      identifiers = ["cloudformation.amazonaws.com"]
-      type        = "Service"
-    }
-  }
-}
-
-resource "aws_iam_role" "AWSCloudFormationStackSetAdministrationRole" {
-  count = var.enable_organization_trail ? 1 : 0
-
-  assume_role_policy = data.aws_iam_policy_document.AWSCloudFormationStackSetAdministrationRole_assume_role_policy[0].json
-  name               = "AWSCloudFormationStackSetAdministrationRole"
-
-  tags = local.tags
-}
-
 resource "aws_cloudformation_stack_set" "permeate_account_policy" {
   count = var.enable_organization_trail ? 1 : 0
 
   name             = "PermeateAccountPolicy"
-  description      = "Creates policies in all accounts of the organinzation for Expel to get basic read permissions of resources in order to investigate alerts"
+  description      = "Creates policies in all accounts of the organization for Expel to get basic read permissions of resources in order to investigate alerts"
   permission_model = "SERVICE_MANAGED"
   capabilities     = ["CAPABILITY_NAMED_IAM"]
 
