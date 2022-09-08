0d0c3d4af57c4d5a4862a07a08f6d5469c9f3576
Ethan Miller <ethan.miller@expel.io>
2022-09-08 11:35:44 -0400

    use existing ct var to know whether to use our own bucket encryption key or default to theirs

--- aws.tf
+++ aws.tf
@@ -18,7 +18,7 @@ locals {
 
   cloudtrail_bucket_arn                = var.existing_cloudtrail_bucket_name != null ? "arn:aws:s3:::${var.existing_cloudtrail_bucket_name}" : "arn:aws:s3:::${var.prefix}-${random_uuid.cloudtrail_bucket_name[0].result}"
   cloudtrail_bucket_name               = var.existing_cloudtrail_bucket_name != null ? var.existing_cloudtrail_bucket_name : "${var.prefix}-${random_uuid.cloudtrail_bucket_name[0].result}"
-  cloudtrail_bucket_encryption_key_arn = var.existing_cloudtrail_kms_key_arn != null ? var.existing_cloudtrail_kms_key_arn : aws_kms_key.cloudtrail_bucket_encryption_key[0].arn
+  cloudtrail_bucket_encryption_key_arn = var.existing_cloudtrail_bucket_name != null ? var.existing_cloudtrail_kms_key_arn : aws_kms_key.cloudtrail_bucket_encryption_key[0].arn
 
   cloudtrail_sns_topic_arn = var.existing_sns_topic_arn == null ? "arn:aws:sns:${local.region}:${local.customer_aws_account_id}:${var.prefix}-${random_uuid.cloudtrail_sns_topic_name[0].result}" : var.existing_sns_topic_arn
 }
