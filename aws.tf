1ef9406b81c56170e666d47337c60a879ba7bd8e
Brandon Achu <brandon.achu@expel.io>
2022-10-03 12:12:45 -0400

    reference current by index

--- aws.tf
+++ aws.tf
@@ -1,7 +1,7 @@
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 data "aws_organizations_organization" "current" {
-    count = var.enable_organization_trail ? 1 : 0
+  count = var.enable_organization_trail ? 1 : 0
 }
 
 locals {
@@ -16,7 +16,7 @@ locals {
 
   customer_aws_account_id      = coalesce(var.expel_customer_aws_account_id, data.aws_caller_identity.current.account_id)
   region                       = data.aws_region.current.name
-  customer_aws_organization_id = try(data.aws_organizations_organization.current.roots[0].id, "")
+  customer_aws_organization_id = try(data.aws_organizations_organization.current[0].roots[0].id, "")
 
   cloudtrail_bucket_arn                = var.existing_cloudtrail_bucket_name != null ? "arn:aws:s3:::${var.existing_cloudtrail_bucket_name}" : "arn:aws:s3:::${var.prefix}-${random_uuid.cloudtrail_bucket_name[0].result}"
   cloudtrail_bucket_name               = var.existing_cloudtrail_bucket_name != null ? var.existing_cloudtrail_bucket_name : "${var.prefix}-${random_uuid.cloudtrail_bucket_name[0].result}"
