6e06496c226326bed5ba0a181eebdf18a0cb24b9
Carlos Barberena <carlos.barberena@expel.io>
2024-04-09 11:23:14 -0400

    WIP: initial push updated docs

--- examples/basic/README.md
+++ examples/basic/README.md
@@ -4,6 +4,23 @@ This Terraform configuration sets up the necessary AWS resources for integrating
 
 The `Basic` configuration is designed for a simple onboarding experience, assuming a single AWS Account is being onboarded with a new CloudTrail entity.
 
+## Variables
+
+- `region`: The AWS region where the CloudTrail resources will be created.
+- `expel_customer_organization_guid`: The organization GUID assigned by Expel for the integration. This GUID can be found in the browser URL after navigating to Settings > My Organization in Workbench.
+
+## Provider
+
+- `aws`: The AWS provider used to create the CloudTrail resources. The region is set based on the value of the "region" variable.
+
+## Module
+
+- `expel_aws_cloudtrail_integration`: The Expel AWS CloudTrail module. It requires several input variables, including the path to the module source code, a map of provider aliases and their corresponding provider configurations, and various settings related to the CloudTrail resources.
+
+## Output
+
+- `expel_aws_cloudtrail_integration`: The output of the `expel_aws_cloudtrail_integration` module. This output can be used to reference the module's resources in other parts of your Terraform configuration.
+
 ## Usage
 
 Follow these steps to deploy the configuration:
--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,51 +1,24 @@
-# This Terraform configuration file sets up an integration with Expel for AWS CloudTrail.
-# It creates an AWS provider, sets up the necessary variables, and configures the Expel AWS CloudTrail module.
-# The module is responsible for creating and managing the CloudTrail resources, such as S3 bucket, SQS queue, and CloudTrail trail.
-# The integration can be customized by modifying the input variables provided to the module.
-
-# Variables:
-# - region: The AWS region where the CloudTrail resources will be created.
-# - expel_customer_organization_guid: The organization GUID assigned by Expel for the integration.
-#   This GUID can be found in the browser URL after navigating to Settings > My Organization in Workbench.
+# Variables
 variable "region" {
   type = string
 }
 
 variable "expel_customer_organization_guid" {
-  description = "Use your organization GUID assigned to you by Expel."
+  description = "Expel organization GUID"
   type        = string
 }
 
-# Provider:
-# - aws: The AWS provider used to create the CloudTrail resources.
-#   The region is set based on the value of the "region" variable.
+# AWS provider
 provider "aws" {
   region = var.region
 }
 
-# Module:
-# - expel_aws_cloudtrail_integration: The Expel AWS CloudTrail module.
-#   It requires the following input variables:
-#   - source: The path to the module source code.
-#   - providers: A map of provider aliases and their corresponding provider configurations.
-#     In this case, the "aws.log_bucket" alias is set to the default AWS provider for the new CloudTrail.
-#   - expel_customer_organization_guid: The organization GUID assigned by Expel.
-#   - enable_organization_trail: Whether to enable organization trail (default: false).
-#   - expel_assume_role_session_name: The session name for assuming the role for CloudTrail access.
-#   - queue_message_retention_days: The number of days to retain messages in the SQS queue.
-#   - enable_sqs_encryption: Whether to enable encryption for the SQS queue.
-#   - enable_cloudtrail_log_file_validation: Whether to enable log file validation for CloudTrail.
-#   - enable_bucket_access_logging: Whether to enable access logging for the S3 bucket.
-#   - enable_access_logging_bucket_encryption: Whether to enable encryption for the access logging bucket.
-#   - enable_bucket_versioning: Whether to enable versioning for the S3 bucket.
-#   - enable_bucket_encryption_key_rotation: Whether to enable rotation of the encryption key for the S3 bucket.
-#   - prefix: The prefix to use for naming the CloudTrail resources.
-#   - tags: A map of tags to apply to the CloudTrail resources.
+# Expel AWS CloudTrail module
 module "expel_aws_cloudtrail_integration" {
   source = "../../"
 
   providers = {
-    aws.log_bucket = aws # setting the log_bucket alias to default aws provider for new cloudtrail
+    aws.log_bucket = aws # Default AWS provider for new CloudTrail
   }
 
   expel_customer_organization_guid = var.expel_customer_organization_guid
@@ -66,7 +39,7 @@ module "expel_aws_cloudtrail_integration" {
   }
 }
 
-# Output:
+# Output
 output "expel_aws_cloudtrail_integration" {
   value = module.expel_aws_cloudtrail_integration
 }
