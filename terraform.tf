f6a5b22efbaee99564dacb87b2e6ecf34f16e785
Brandon Achu <brandon.achu@expel.io>
2024-04-24 12:49:21 -0400

    update version req

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.46"
+      version = "~> 5.0"
       # For new & existing cloudtrail with resources in single aws account, set the log_bucket alias to default aws provider.
       # For existing cloudtrail with resources in different aws accounts, create an aws provider for the log_bucket account & pass it's alias.
       # See examples for reference.
