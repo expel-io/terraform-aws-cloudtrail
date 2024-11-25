4da49209284bd11a25f55ae93ea08914beab0837
Grace <53713122+Xxyumi-hub@users.noreply.github.com>
2024-11-25 11:38:50 -0500

    TOG-558: remove DescribeOrg perm as its no longer needed

--- iam.tf
+++ iam.tf
@@ -153,7 +153,6 @@ data "aws_iam_policy_document" "cloudtrail_manager_iam_document" {
       "lambda:ListFunctions",
       "lightsail:GetInstances",
       "lightsail:GetRegions",
-      "organizations:DescribeOrganization",
       "organizations:ListAccounts",
       "rds:DescribeDBInstances",
       "rds:ListTagsForResource",
--- stackset_template.tf
+++ stackset_template.tf
@@ -77,7 +77,6 @@ locals {
                                 "lambda:ListFunctions",
                                 "lightsail:GetInstances",
                                 "lightsail:GetRegions",
-                                "organizations:DescribeOrganization",
                                 "organizations:ListAccounts",
                                 "rds:DescribeDBInstances",
                                 "rds:ListTagsForResource",
